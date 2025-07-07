c03b90f1550b0333b8bc67cfddd8cd4f8f3d63ec
akram01ali <akramaliluch64@gmail.com>
2025-07-07 11:22:15 +0200

    Navigation

--- apps/smartstock-mobile/types/navigation.tsx
+++ apps/smartstock-mobile/types/navigation.tsx
@@ -1,8 +1,10 @@
 export type RootStackParamList = {
   Home: undefined;
+  Inventory: undefined;
   MyPath: undefined;
   DocumentCategories: undefined;
   Wiki: undefined;
   Settings: undefined;
   Login: undefined;
+  Register: undefined;
 };
